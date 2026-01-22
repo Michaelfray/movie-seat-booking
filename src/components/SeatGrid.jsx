9dc9ae64ff15b7a593b59ae5ea6a8d61465e811b
Michael Fray <michaelfray1@gmail.com>
2026-01-22 23:04:20 +0100

    Took SeatGrid.jsx to src/components/

---
+++ src/components/SeatGrid.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function SeatGrid({ handleCount }) {
+  const rows = 6;
+  const seatsPerRow = 8;
+
+  const occupiedSeats = [
+    "1-4",
+    "1-5",
+    "2-7",
+    "2-8",
+    "4-4",
+    "4-5",
+    "5-5",
+    "5-6",
+  ];
+
+  const [selectedSeats, setSelectedSeats] = useState([]);
+
+  const handleSeatClick = (rowIndex, seatIndex) => {
+    const seatId = `${rowIndex}-${seatIndex}`;
+
+    if (occupiedSeats.includes(seatId)) return;
+
+    if (selectedSeats.includes(seatId)) {
+      setSelectedSeats(selectedSeats.filter((s) => s !== seatId));
+    } else {
+      setSelectedSeats([...selectedSeats, seatId]);
+    }
+  };
+
+  return (
+    <div className="seat-grid">
+      {[...Array(rows)].map((_, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {[...Array(seatsPerRow)].map((_, seatIndex) => {
+            const seatId = `${rowIndex}-${seatIndex}`;
+            const isSelected = selectedSeats.includes(seatId);
+            const isOccupied = occupiedSeats.includes(seatId);
+
+            return (
+              <div
+                key={seatIndex}
+                className={`seat 
+                ${isSelected ? "selected" : ""} 
+                ${isOccupied ? "occupied" : ""}
+              `}
+                onClick={() => {
+                  handleSeatClick(rowIndex, seatIndex);
+                  handleCount(!isSelected);
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+}
