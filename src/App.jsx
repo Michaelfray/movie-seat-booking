169a7abd8acf0f8c2e1f1967877db24949f15d4b
Michael Fray <michaelfray1@gmail.com>
2026-01-27 16:03:29 +0100

    added Movie.js and updated App.jsx price/total logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import SeatGrid from "./components/SeatGrid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [price, setPrice] = useState(100);
+  const total = count * price;
+
+  useEffect(() => {
+    console.log(total);
+  }, [total]);
 
   function handleCount(selected) {
     if (selected) {
@@ -17,7 +23,12 @@ function App() {
     <>
       <div className="movie-container">
         <label htmlFor="movie">Pick a movie:</label>
-        <select name="movie" id="movie">
+        <select
+          name="movie"
+          id="movie"
+          // defaultValue="100"
+          onChange={(e) => setPrice(Number(e.target.value))}
+        >
           <option value="100">Fast and furious 6 (100 kr)</option>
           <option value="50">The mummy returns (50 kr)</option>
           <option value="70">Jumanji: Welcome to the Jungle (70 kr)</option>
@@ -113,7 +124,7 @@ function App() {
 
       <p className="text">
         You have selected <span id="count">{count}</span> seats for a price of $
-        <span id="total">{0}</span>
+        <span id="total">{total}</span>
       </p>
     </>
   );
